a8dc14403425bd4ed349115dd7d180a5214221d6
Calvin <cho327@bu.edu>
2019-10-14 21:40:57 -0400

    incomplete with errors

--- routes/ps3.js
+++ routes/ps3.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('Test', { title: 'Im in'});
+    res.render('title', { title: 'Im in'});
 });
 
 module.exports = router;
