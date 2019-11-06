37c8a2e7e578a5b2e093733acd4c5ac994407c78
Calvin <cho327@bu.edu>
2019-11-06 15:55:10 -0500

    got the postman code lol

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('ps4',ps4router);
+app.use('/ps4',ps4router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/ps4.js
+++ routes/ps4.js
@@ -17,18 +17,28 @@ console.log('This is the URL we are using Get to : ' + responseUrl);
 
 /* GET users listing. */
 router.get('/get-weather', function(req, res, next) {
-    fetch(responseUrl)
-        .then(res => res.json())
-        .then(data => {
-            res.send({ data });
-        })
-        .catch(err => {
-            res.redirect('/error');
-            res.send('respond with a resource');
-        })
+    const request = require("request");
+
+    const options = { method: 'GET',
+        url: 'http://api.weatherunlocked.com/api/current/us.02215',
+        qs:
+            { app_id: 'f8e83847',
+                app_key: 'abf793dea5efeee55e23c588ffe173a4' },
+        headers:
+            { 'cache-control': 'no-cache',
+                Connection: 'keep-alive',
+                'Accept-Encoding': 'gzip, deflate',
+                Host: 'api.weatherunlocked.com',
+                'Postman-Token': 'd5030d25-8b68-4943-8ff1-497a6eaaa384,96fb1875-be2d-42a2-9071-37fc375c6730',
+                'Cache-Control': 'no-cache',
+                Accept: '*/*',
+                'User-Agent': 'PostmanRuntime/7.19.0' } };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+
+        res.send(body);
+    });
 });
-
-
-
 
 module.exports = router;
